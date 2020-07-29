45f9736fed96b2bea575917e9a41bc73f624727f
Recep Aslantas <info@recp.me>
2020-07-30 00:09:07 +0300

    MetalKit helpers to make it easier to integrate/wrap MetalKit content with GPU library

---
+++ include/gpu/metalkit.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2020 Recep Aslantas
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef gpu_metalkit_helpers_h
+#define gpu_metalkit_helpers_h
+
+#if defined(__APPLE__) && defined(__OBJC__)
+#import <MetalKit/MetalKit.h>
+
+GPU_INLINE
+void
+GPUDrawIndexedMTKSubMesh(GPURenderCommandEncoder *rce,
+                         MTKSubmesh              *submesh) {
+  GPUDrawIndexed(rce,
+                 (GPUPrimitiveType)submesh.primitiveType,
+                 (uint32_t)submesh.indexCount,
+                 (GPUIndexType)submesh.indexType,
+                 (GPUBuffer *)submesh.indexBuffer.buffer,
+                 (uint32_t)submesh.indexBuffer.offset);
+}
+
+GPU_INLINE
+void
+GPUSetVertexBuffersFromMTKMeshBuffers(GPURenderCommandEncoder  *rce,
+                                      NSArray<MTKMeshBuffer *> *buffers) {
+  MTKMeshBuffer *buffer;
+  for (uint32_t i = 0; i < buffers.count; i++) {
+    buffer = buffers[i];
+    if ((NSNull*)buffer != [NSNull null])
+      GPUSetVertexBuffer(rce, (GPUBuffer *)buffer.buffer, buffer.offset, i);
+  }
+}
+#endif
+
+#endif /* gpu_metalkit_helpers_h */
